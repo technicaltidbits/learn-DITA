92f73771e5f639ac0c910cc1a099225d1112590e
technicaltidbits <deanna.thompson734@outlook.com>
2020-03-27 23:47:13 -0400

    Add config file and overview page

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Learn DITA Basics',
+    description: 'This site teaches you the basics of DITA, or the Darwin Information Typing Architecture',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+
+        ]
+    }
+}
